6dc6921af558a6dfaed6d79e736911f9bb0fa75a
James Cox <jamesncox@yahoo.com>
2019-06-12 20:38:16 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,16 @@ class App < Sinatra::Base
   end 
   
   post '/teams' do
-  
+    @team_name = params[:team][:name]
+    @team_motto = params[:team][:motto]
+    @hero_name = []
+    @hero_power = []
+    @hero_bio = []
+    @team_members = params[:team][:members]
+    @team_members.each do |hero|
+      @hero_name << hero[:name]
+      @hero_power << hero[:power]
+      @hero_bio << hero[:bio]
   erb :team  
   end 
 end
